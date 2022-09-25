6a6b9a6d938deb6ca1f7f6d2c7b8247117e768b0
Cmanka <offatrubkin@gmail.com>
2022-09-25 12:03:09 +0300

    updated button

--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -2,9 +2,23 @@ import styled from 'styled-components';
 import { CssProps } from './type';
 
 export const ButtonStyled = styled.button<CssProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: ${({ direction }) => direction};
+  gap: ${({ gap }) => gap};
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
   border: ${({ border }) => border};
   color: ${({ color }) => color};
   margin: ${({ margin }) => margin};
   padding: ${({ padding }) => padding};
   background: ${({ background }) => background};
+  font-size: ${({ fontSize }) => fontSize};
+
+  & > img {
+    width: ${({ imgWidth }) => imgWidth};
+    height: ${({ imgHeight }) => imgHeight};
+    object-fit: ${({ imgFit }) => imgFit};
+  }
 `;
--- src/components/Button/type.ts
+++ src/components/Button/type.ts
@@ -1,11 +1,19 @@
 import { CSSProperties, HTMLAttributes, PropsWithChildren } from 'react';
 
 export interface CssProps {
-  border: CSSProperties['border'];
-  color: CSSProperties['color'];
-  margin: CSSProperties['margin'];
-  padding: CSSProperties['padding'];
-  background: CSSProperties['background'];
+  direction?: CSSProperties['flexDirection'];
+  border?: string;
+  color?: string;
+  margin?: string;
+  padding?: string;
+  background?: string;
+  gap?: string;
+  width?: string;
+  height?: string;
+  imgHeight?: string;
+  imgWidth?: string;
+  imgFit?: CSSProperties['objectFit'];
+  fontSize?: string;
 }
 
 export interface ButtonProps extends PropsWithChildren, Omit<HTMLAttributes<HTMLButtonElement>, 'color'>, CssProps {
